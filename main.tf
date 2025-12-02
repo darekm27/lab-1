1bfb36d30b20651abe58db3b0065415dc294c7b5
darekm <56674649+darekm27@users.noreply.github.com>
2025-12-02 14:47:35 +0000

    Advanced validation

--- main.tf
+++ main.tf
@@ -2,6 +2,18 @@ variable "server-name" {
   type        = string
   description = "Name of server to provision"
 }
+locals {
+  minNumberOfServers = 1
+  maxNumberOfServers = 10
+}
+variable "number-of-servers" {
+  type = number
+  description = "Required number of servers"
+  validation {
+    condition = var.number-of-servers >= local.minNumberOfServers && var.number-of-servers <= local.maxNumberOfServers
+    error_message = "Number of servers must be in range ${local.minNumberOfServers} - ${local.maxNumberOfServers}"
+  }
+}
 
 output "output" {
   value = var.server-name
