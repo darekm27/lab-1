12ac4554af22deddd67c68a93f54dc76c0d3d40a
darekm <56674649+darekm27@users.noreply.github.com>
2025-12-17 10:16:48 +0000

    Import state from AWS

---
+++ aws3/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = "~>1.14.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.25.0"
+    }
+  }
+}
+
+provider "aws" {
+
+}
+
+
+# import {
+#   to = aws_s3_bucket.to_import
+#   id = data.aws_s3_bucket.to_import.id
+# }
+
+# data "aws_s3_bucket" "to_import" {
+#   bucket = "awsninja5-to-import-1"
+
+# }
