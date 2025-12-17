19038c4beba9bc4a6ec4eb113d6ee0b6084840d3
darekm <56674649+darekm27@users.noreply.github.com>
2025-12-17 09:48:39 +0000

    Create EC2 and VPC

--- aws2/main.tf
+++ aws2/main.tf
@@ -5,10 +5,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.25.0"
     }
-    http = {
-      source  = "hashicorp/http"
-      version = "3.5.0"
-    }
   }
 }
 
@@ -16,20 +12,54 @@ provider "aws" {
 
 }
 
-provider "http" {
-
+variable "name_prefix" {
+  type    = string
+  default = "awsninja5"
 }
 
-data "http" "terraform_version" {
-  url = "https://checkpoint-api.hashicorp.com/v1/check/terraform"
+variable "server_version" {
+  type = string
+  validation {
+    condition = var.server_version == "1" || var.server_version == "2"
+    error_message = "Wrong version"
+  }
+}
+
+data "aws_ami" "server_ami" {
+  filter {
+    name = "name"
+    values = [ "ubuntu-linux-apache-${var.server_version}-*" ]
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.server_ami.id
+  instance_type = "t2.nano"
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+  tags = {
+    Name = "${var.name_prefix}-app-server"
+  }
+}
+
+resource "aws_security_group" "allow_all" {
+  name = "${var.name_prefix}-public-accaess"
+  ingress {
+      from_port   = 0
+      to_port     = 0
+      protocol    = -1
+      cidr_blocks = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = [ "::/0" ]
+    }
   
+  egress {
+      from_port   = 0
+      to_port     = 0
+      protocol    = -1
+      cidr_blocks = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = [ "::/0" ]
+    }
 }
 
-locals {
-  terraform_info = jsondecode(data.http.terraform_version.response_body)
+output "server_ip" {
+  value = aws_instance.app_server.public_ip
 }
-
-output "info" {
-  value = "Terraform ${local.terraform_info.current_version} (${local.terraform_info.current_release})"
-}
-
