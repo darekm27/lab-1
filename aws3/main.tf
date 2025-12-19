1da22acad4ea44b713dec695331ff408e0ce73d2
darekm <56674649+darekm27@users.noreply.github.com>
2025-12-19 11:18:27 +0000

    Import configuration

--- aws3/main.tf
+++ aws3/main.tf
@@ -13,12 +13,24 @@ provider "aws" {
 }
 
 
-# import {
-#   to = aws_s3_bucket.to_import
-#   id = data.aws_s3_bucket.to_import.id
-# }
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "6.5.1"
+  name = "awsninja5-vpc"
+  cidr = "10.0.0.0/16"
 
-# data "aws_s3_bucket" "to_import" {
-#   bucket = "awsninja5-to-import-1"
+  azs                     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets         = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets          = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  map_public_ip_on_launch = true
 
-# }
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+
+}
