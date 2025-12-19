c3c854f9c1e362a53536dfc3688c09688b532295
darekm <56674649+darekm27@users.noreply.github.com>
2025-12-19 11:37:08 +0000

    Create multiple EC2

--- aws2/main.tf
+++ aws2/main.tf
@@ -12,6 +12,10 @@ provider "aws" {
 
 }
 
+locals {
+  tag_key_name = "${var.name_prefix}-app-server"
+}
+
 variable "name_prefix" {
   type    = string
   default = "awsninja5"
@@ -20,46 +24,56 @@ variable "name_prefix" {
 variable "server_version" {
   type = string
   validation {
-    condition = var.server_version == "1" || var.server_version == "2"
+    condition     = var.server_version == "1" || var.server_version == "2"
     error_message = "Wrong version"
   }
 }
 
+variable "number_of_servers" {
+  type    = number
+  default = 2
+}
+
 data "aws_ami" "server_ami" {
   filter {
-    name = "name"
-    values = [ "ubuntu-linux-apache-${var.server_version}-*" ]
+    name   = "name"
+    values = ["ubuntu-linux-apache-${var.server_version}-*"]
   }
 }
 
 resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.server_ami.id
-  instance_type = "t2.nano"
+  ami                    = data.aws_ami.server_ami.id
+  instance_type          = "t3.micro"
+  count                  = var.number_of_servers
   vpc_security_group_ids = [aws_security_group.allow_all.id]
   tags = {
-    Name = "${var.name_prefix}-app-server"
+    Name = "${local.tag_key_name}-${count.index}"
   }
 }
 
 resource "aws_security_group" "allow_all" {
   name = "${var.name_prefix}-public-accaess"
   ingress {
-      from_port   = 0
-      to_port     = 0
-      protocol    = -1
-      cidr_blocks = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = [ "::/0" ]
-    }
-  
+    from_port        = 0
+    to_port          = 0
+    protocol         = -1
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   egress {
-      from_port   = 0
-      to_port     = 0
-      protocol    = -1
-      cidr_blocks = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = [ "::/0" ]
-    }
+    from_port        = 0
+    to_port          = 0
+    protocol         = -1
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
 
 output "server_ip" {
-  value = aws_instance.app_server.public_ip
+  value = formatlist(
+    "%s - %s",
+    aws_instance.app_server[*].tags["Name"],
+    aws_instance.app_server[*].public_ip
+  )
 }
